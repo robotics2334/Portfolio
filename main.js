8b1f3c03462cde7fde02251656adcbb02f5969c8
Kanchan Patil <157134330+robotics2334@users.noreply.github.com>
2024-01-26 13:00:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+var typed= new typed(".text",{
+    strings: ["Fronted Developer", " web Developer"],
+    typespeed: 100,
+    backspeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+<script src="circle-progress.js"></script>
